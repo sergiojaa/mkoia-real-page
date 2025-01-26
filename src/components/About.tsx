d864b987d19528ab5a05724284c81089bf41ef06
seriojaa <kaliashvilisergi@gmail.com>
2025-01-26 19:34:05 +0400

    complete design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,45 +4,42 @@ import image4 from '../assets/logistic4.jpg';
 export default function About() {
   return (
     <div>
-      
 
-      {/* Section with image3 as the background */}
+
       <div
-        className="lg:flex  md:mt-[180px]   items-center bg-cover bg-center"
-        // style={{ backgroundImage: `url(${image5})`, height: '775px' }}
+        className="lg:flex-row items-center  flex flex-col  justify-center  md:mt-[180px] mx-auto   items-center bg-cover bg-center"
       >
-        
-        <div className="lg:ml-[20px]   md:ml-[20px] lg:mt-[100px] ">
-        <h1 className="text-blue-500  xl:pb-[30px] font-bold xl:pt-[170px] lg:mt-[-250px] xl:ml-[150px]    mt-[170px] font-lato text-[25px] ml-[20px] md:text-[30px] lg:text-[40px]">ჩვენს შესახებ</h1>
 
-          {/* <h1 className="text-gray-500  font-bold xl:mt-[1px] lg:mt-[-250px] xl:ml-[150px]    mt-[170px] font-lato text-[15px] ml-[20px] md:text-[30px] lg:text-[30px]">ვინ ვართ ჩვენ</h1> */}
-          <p className="   text-gray-500 ml-[20px] lg:w-[500px] xl:ml-[150px] lg:text-[20px] md:w-[700px] font-lato md:text-[30px] md: mt-[10px]">
+        <div className="lg:ml-[20px]   md:ml-[20px] lg:mt-[250px] ">
+          <h1 className="text-blue-500  xl:pb-[30px] font-bold xl:pt-[50px] lg:mt-[-250px] xl:ml-[100px]    mt-[170px] font-lato text-[25px] ml-[20px] md:text-[30px] lg:text-[40px]">ჩვენს შესახებ</h1>
+
+          <p className="   text-gray-500 ml-[20px] lg:w-[500px] xl:ml-[100px] lg:text-[20px] md:w-[700px] font-lato md:text-[30px] md: mt-[10px]">
             <span className='font-bold text-blue-500'>ლოჯისტიკური კომპანია ემკოია</span> -  ფლობს საწყობს თბილისში, საიდანაც ახდენს ყველაფრის მიწოდებას უმოკლეს ვადაში.
           </p>
-          <h2 className="text-blue-500 xl:ml-[150px] font-lato ml-[20px] lg:text-[24px] mt-[15px] font-bold md:text-[40px] md:mt-[20px]">ემკოიას აქვს ორი მთავარი ნიშა:</h2>
+          <h2 className="text-blue-500 xl:ml-[100px] font-lato ml-[20px] lg:text-[24px] mt-[15px] font-bold md:text-[40px] md:mt-[20px]">ემკოიას აქვს ორი მთავარი ნიშა:</h2>
           <div >
-            <ul className=" xl:ml-[130px]  font-lato md:text-[30px] text-gray-500 text-[20px] mt-[10px]">  
+            <ul className=" xl:ml-[80px]  font-lato md:text-[30px] text-gray-500 text-[20px] mt-[10px]">
               <li className='mb-[10px] xl:text-[30px] lg:text-[23px]  ml-[20px]'>დროული მიწოდება</li>
               <li className='ml-[20px] xl:text-[30px] lg:text-[23px]  '>მომხმარებელზე გათვლილი  მომსახურება</li>
             </ul>
-            {/* <p className="mb-[20px]">ლოჯისტიკური სერვისი</p>
-            <p>მომხმარებელზე გათვლილი მომსახურება</p> */}
+
           </div>
         </div>
-       <div>
-        <img className='xl:w-[600px] p-[20px] xl:ml-[80px] xl:mt-[50px]' src={image4} alt="" />
-       </div>
-      </div>
-      <div className="xl:mt-[20px]">
-  <p className="text-gray-500 lg:w-[300px] xl:text-[20px] xl:ml-[160px] xl:w-[600px] lg:ml-[-30px] mb-[20px] md:ml-[40px] lg:text-[20px] mt-[20px] lg:mt-[-10px] ml-[20px] font-lato md:text-[20px] md:w-[600px] md:mt-[50px]">
-    <span className="text-blue-500 font-bold lg:text-[24px] text-[25px] font-lato md:text-[39px]">
-    ლოჯისტიკური კომპანია ემკოია - 
-    </span> 
-    სპეციალიზირებული სამშენებლო მასალების ლოჯისტიკაში. ჩვენი მთავარი ვალდებულებაა, რომ თქვენი პროექტები დასრულდეს დროულად . კომპანიის ნიშაა პროდუქტის მიწოდება იმავე დღეს, არანაირი გადავადება. კომპანიისთვის უპირველესია დრო, შესაბამისად დაუღალავად ვშრომობთ თქვენი საჭიროებების დასაკმაყოფილებლად. ასევე კომპანიას აქვს საშუალება ჩამოგიტანოთ ნებისმიერი სამშენებლო მასალა ჩინეთიდან და თურქეთიდან. ვმუშაობთ როგორც მსხვილ კონტრაქტორებზე ასევე კერძო პირებზე. შესაკვეთად დაგვიკავშირდით მითითებულ მონაცემებზე. <br />
-    <span className='font-bold text-blue-500 lg:text-[24px]'>შპს „ემკოია“ </span> - თანამშრომლობს ისეთ კომპანიებთან როგორებიც არიან: „დელტა მშენებელი“, „KTW DEVELOPEMNT“, “Coca-Cola Bottled Georgia”, „Rebuild“, „Geo Group“, „Construction Group“. ვპარტნიორობთ თურქულ და ჩინურ კომპანიებთან.  </p>
-</div>
+        <div className="flex px-5 py-12 justify-center items-center w-full">
+          <img className="xl:w-[550px]  w-[400px] xl:ml-[80px] xl:mt-[50px]" src={image4} alt="Logistics" />
+        </div>
 
-     
+      </div>
+      <div className="xl:mt-[20px] lg:ml-[70px] mt-10 ">
+        <p className="text-gray-500 lg:w-[300px] lg:ml-10 lg:mt-5  xl:text-[20px] xl:ml-[50px] xl:w-[600px] lg:ml-[-30px] mb-[20px] md:ml-[40px] lg:text-[20px] mt-[20px] lg:mt-[-10px] ml-[20px] font-lato md:text-[20px] md:w-[600px] md:mt-[50px]">
+          <span className="text-blue-500 font-bold lg:text-[24px] text-[25px] font-lato md:text-[39px]">
+            ლოჯისტიკური კომპანია ემკოია -
+          </span>
+          სპეციალიზირებული სამშენებლო მასალების ლოჯისტიკაში. ჩვენი მთავარი ვალდებულებაა, რომ თქვენი პროექტები დასრულდეს დროულად . კომპანიის ნიშაა პროდუქტის მიწოდება იმავე დღეს, არანაირი გადავადება. კომპანიისთვის უპირველესია დრო, შესაბამისად დაუღალავად ვშრომობთ თქვენი საჭიროებების დასაკმაყოფილებლად. ასევე კომპანიას აქვს საშუალება ჩამოგიტანოთ ნებისმიერი სამშენებლო მასალა ჩინეთიდან და თურქეთიდან. ვმუშაობთ როგორც მსხვილ კონტრაქტორებზე ასევე კერძო პირებზე. შესაკვეთად დაგვიკავშირდით მითითებულ მონაცემებზე. <br />
+          <span className='font-bold text-blue-500 lg:text-[24px]'>შპს „ემკოია“ </span> - თანამშრომლობს ისეთ კომპანიებთან როგორებიც არიან: „დელტა მშენებელი“, „KTW DEVELOPEMNT“, “Coca-Cola Bottled Georgia”, „Rebuild“, „Geo Group“, „Construction Group“. ვპარტნიორობთ თურქულ და ჩინურ კომპანიებთან.  </p>
+      </div>
+
+
 
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,17 @@
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.jpg';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faLanguage } from '@fortawesome/free-solid-svg-icons';
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.jpg";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-  const location = useLocation()
-  console.log(location.pathname)
+  const location = useLocation();
 
-  
-  const navLinkColor = location.pathname == '/'  ? 'text-white' : 'text-blue-500';
-  const hamburgerColor = location.pathname == '/'  ? 'bg-white' : 'bg-gray-500';
-  // const languageColor = isHome ? 'text-white' : 'text-blue-500'; // Set the same logic for language icon and span
+  const navLinkColor =
+    location.pathname === "/" ? "text-white" : "text-blue-500";
+  const hamburgerColor =
+    location.pathname === "/" ? "bg-white" : "bg-gray-500";
 
   useEffect(() => {
     const handleResize = () => {
@@ -25,95 +21,98 @@ export default function Header() {
       }
     };
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, [isMobile]);
 
   return (
     <header className="absolute top-0 left-0 w-full z-10 bg-transparent">
-      <div className='flex items-center justify-between max-w-screen-xl mx-auto px-4'>
-        <div className='flex items-center mt-[20px] justify-between w-full'>
-          <Link to={'/'}  >
-            <img className='cursor-pointer xl:ml-[100px] w-[80px] ml-[50px]' src={logo} alt="Logo" />
+      <div className="flex items-center justify-between max-w-screen-xl mx-auto px-4">
+        <div className="flex items-center mt-[20px] justify-between w-full">
+          <Link to={"/"}>
+            <img
+              className="cursor-pointer xl:ml-[100px] w-[80px] ml-[50px]"
+              src={logo}
+              alt="Logo"
+            />
           </Link>
 
-          {/* Hamburger icon and language icon (always visible) */}
-          <div className="flex items-center">
-            <div
-              className='lg:hidden ml-4 cursor-pointer'
-              onClick={() => setMenuOpen(!menuOpen)}
-            >
-              <div className={`h-1 w-8 mb-1 ${hamburgerColor}`}></div>
-              <div className={`h-1 w-8 mb-1 ${hamburgerColor}`}></div>
-              <div className={`h-1 w-8 ${hamburgerColor}`}></div>
-            </div>
-
-            {/* Language Icon (Always visible) */}
-            {/* <div className="ml-4 md:ml-[800px]">
-              <span className={` pr-[5px] text-[20px] font-lato ${languageColor}`}>ქა</span>
-              <FontAwesomeIcon icon={faLanguage} className={`text-[20px] ${languageColor}`} />
-            </div> */}
+          {/* Hamburger icon */}
+          <div
+            className="md:hidden ml-4 cursor-pointer z-20"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <div className={`h-1 w-8 mb-1 ${hamburgerColor}`}></div>
+            <div className={`h-1 w-8 mb-1 ${hamburgerColor}`}></div>
+            <div className={`h-1 w-8 ${hamburgerColor}`}></div>
           </div>
         </div>
 
+        {/* Mobile menu */}
         {menuOpen && isMobile && (
-          <div className='absolute top-1 left-5 w-full bg-transparent flex flex-col items-center'>
+          <div
+            className={`fixed top-0 right-0 h-full w-[200px] bg-blue-500 flex flex-col items-center py-10 space-y-6 text-white shadow-lg z-10 transition-transform duration-500 ease-in-out ${menuOpen ? "translate-x-0" : "translate-x-full"
+              }`}
+          >
             <Link
-              to='/'
-              onClick={() => {  setMenuOpen(false); }}
-              className={`w-full text-center p-1 mb-1 ${navLinkColor} transition duration-300 hover:bg-gray-200`}
+              to="/"
+              onClick={() => setMenuOpen(false)}
+              className="text-lg font-semibold transition duration-300 hover:text-gray-200"
             >
               Home
             </Link>
             <Link
-              to='/about'
-              onClick={() => {  setMenuOpen(false); }}
-              className={`w-full text-center p-1 mb-1 ${navLinkColor} transition duration-300 hover:bg-gray-200`}
+              to="/about"
+              onClick={() => setMenuOpen(false)}
+              className="text-lg font-semibold transition duration-300 hover:text-gray-200"
             >
               About
             </Link>
             <Link
-              to='/services'
-              onClick={() => {  setMenuOpen(false); }}
-              className={`w-full text-center p-1 mb-1 ${navLinkColor} transition duration-300 hover:bg-gray-200`}
+              to="/services"
+              onClick={() => setMenuOpen(false)}
+              className="text-lg font-semibold transition duration-300 hover:text-gray-200"
             >
               Services
             </Link>
             <Link
-              to='/contact'
-              onClick={() => {  setMenuOpen(false); }}
-              className={`w-full text-center p-1 mb-1 ${navLinkColor} transition duration-300 hover:bg-gray-200`}
+              to="/contact"
+              onClick={() => setMenuOpen(false)}
+              className="text-lg font-semibold transition duration-300 hover:text-gray-200"
             >
               Contact
             </Link>
           </div>
         )}
 
-        
+        {/* Desktop menu */}
         {!menuOpen && (
-          <div className='items-center lg:mr-[100px]  ml-6 hidden lg:flex'>
+          <div className="items-center lg:mr-[100px] ml-6 hidden md:flex">
             <Link
-              to='/'
-              
-              className={`font-poppins ${navLinkColor} mr-6 text-xl pb-1 transition duration-300 hover:text-gray-400 ${location.pathname  === '/' ? 'border-b-2 border-white' : ''}`}
+              to="/"
+              className={`font-poppins ${navLinkColor} mr-6 text-xl pb-1 transition duration-300 hover:text-gray-400 ${location.pathname === "/" ? "border-b-2 border-white" : ""
+                }`}
             >
               Home
             </Link>
             <Link
-              to='/about'
-              className={`font-poppins ${navLinkColor} text-xl pb-1 mr-6 transition duration-300 hover:text-gray-400 ${location.pathname  === '/about' ? 'border-b-2 border-blue' : ''}`}
+              to="/about"
+              className={`font-poppins ${navLinkColor} text-xl pb-1 mr-6 transition duration-300 hover:text-gray-400 ${location.pathname === "/about" ? "border-b-2 border-blue" : ""
+                }`}
             >
               About
             </Link>
             <Link
-              to='/services'
-              className={`font-poppins ${navLinkColor} text-xl pb-1 mr-6 transition duration-300 hover:text-gray-400 ${location.pathname  === '/services' ? 'border-b-2 border-blue' : ''}`}
+              to="/services"
+              className={`font-poppins ${navLinkColor} text-xl pb-1 mr-6 transition duration-300 hover:text-gray-400 ${location.pathname === "/services" ? "border-b-2 border-blue" : ""
+                }`}
             >
               Services
             </Link>
             <Link
-              to='/contact'
-              className={`font-poppins ${navLinkColor} text-xl pb-1 transition duration-300 hover:text-gray-400 ${location.pathname  === '/contact' ? 'border-b-2 border-blue' : ''}`}
+              to="/contact"
+              className={`font-poppins ${navLinkColor} text-xl pb-1 transition duration-300 hover:text-gray-400 ${location.pathname === "/contact" ? "border-b-2 border-blue" : ""
+                }`}
             >
               Contact
             </Link>
@@ -121,7 +120,9 @@ export default function Header() {
         )}
       </div>
 
-      {location.pathname  !== '/' && <div className='mt-[20px] h-[1px] w-full bg-gray-500'></div>}
+      {location.pathname !== "/" && (
+        <div className="mt-[20px] h-[1px] w-full bg-gray-500"></div>
+      )}
     </header>
   );
 }
